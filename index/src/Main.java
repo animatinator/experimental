c4fad8f2a2954046d21c9569c452b23204d26fd6
animatinator <animatinator@gmail.com>
2018-06-09 11:44:52 +0100

    Add test class

---
+++ index/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Testing VCS setup");
+        for (String arg : args) {
+            System.out.println(arg);
+        }
+    }
+}
