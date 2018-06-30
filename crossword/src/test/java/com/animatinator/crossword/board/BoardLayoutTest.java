676ff9a70671a76713f593927e6bc79ba5dabf8a
animatinator <animatinator@gmail.com>
2018-06-30 14:06:53 +0100

    Add a whole load more tests, and a couple for existing failure cases
    The failure cases will be investigated in a follow-up. See paste
    links for the corresponding problematic generation steps.

--- crossword/src/test/java/com/animatinator/crossword/board/BoardLayoutTest.java
+++ crossword/src/test/java/com/animatinator/crossword/board/BoardLayoutTest.java
@@ -79,4 +79,138 @@ public class BoardLayoutTest {
         LaidWord wordToLay = new LaidWord("hello", new BoardPosition(0, 0), Direction.HORIZONTAL);
         assertFalse(layout.isAdjacentToExistingWord(wordToLay));
     }
+
+    /**
+     * A word legally overlapping an existing word is valid.
+     */
+    @Test
+    public void isAdjacentToExistingWord_overlapsCorrectly() {
+        BoardLayout layout = new BoardLayout(5, 3);
+        layout.copyLayoutFromStringArray(new String[][]{
+                ".pie.".split(""),
+                ".....".split(""),
+                ".....".split(""),
+        });
+        LaidWord wordToLay = new LaidWord("ice", new BoardPosition(2, 0), Direction.VERTICAL);
+        assertFalse(layout.isAdjacentToExistingWord(wordToLay));
+    }
+
+    /**
+     * A word legally overlapping an existing word is valid, even if its letters don't match. Letter matching is not
+     * handled by this method.
+     */
+    @Test
+    public void isAdjacentToExistingWord_overlapsCorrectly_badIntersection() {
+        BoardLayout layout = new BoardLayout(5, 3);
+        layout.copyLayoutFromStringArray(new String[][]{
+                ".pie.".split(""),
+                ".....".split(""),
+                ".....".split(""),
+        });
+        LaidWord wordToLay = new LaidWord("pie", new BoardPosition(2, 0), Direction.VERTICAL);
+        assertFalse(layout.isAdjacentToExistingWord(wordToLay));
+    }
+
+    @Test
+    public void isAdjacentToExistingWord_triviallyTrue() {
+        BoardLayout layout = new BoardLayout(5, 3);
+        layout.copyLayoutFromStringArray(new String[][]{
+                ".pie.".split(""),
+                ".....".split(""),
+                ".....".split(""),
+        });
+        LaidWord wordToLay = new LaidWord("pie", new BoardPosition(1, 1), Direction.HORIZONTAL);
+        assertTrue(layout.isAdjacentToExistingWord(wordToLay));
+    }
+
+    @Test
+    public void isAdjacentToExistingWord_onlyJust() {
+        BoardLayout layout = new BoardLayout(6, 3);
+        layout.copyLayoutFromStringArray(new String[][]{
+                "......".split(""),
+                ".pie..".split(""),
+                "......".split(""),
+        });
+        LaidWord wordToLay = new LaidWord("pie", new BoardPosition(3, 0), Direction.HORIZONTAL);
+        assertTrue(layout.isAdjacentToExistingWord(wordToLay));
+    }
+
+    @Test
+    public void isAdjacentToExistingWord_diagonalMiss() {
+        BoardLayout layout = new BoardLayout(5, 4);
+        layout.copyLayoutFromStringArray(new String[][]{
+                ".pie.".split(""),
+                ".....".split(""),
+                ".....".split(""),
+                ".....".split(""),
+        });
+        LaidWord wordToLay = new LaidWord("pie", new BoardPosition(0, 1), Direction.VERTICAL);
+        assertFalse(layout.isAdjacentToExistingWord(wordToLay));
+    }
+
+    @Test
+    public void isAdjacentToExistingWord_validCorner() {
+        BoardLayout layout = new BoardLayout(5, 3);
+        layout.copyLayoutFromStringArray(new String[][]{
+                ".pie.".split(""),
+                ".....".split(""),
+                ".....".split(""),
+        });
+        LaidWord wordToLay = new LaidWord("eel", new BoardPosition(3, 0), Direction.VERTICAL);
+        assertFalse(layout.isAdjacentToExistingWord(wordToLay));
+    }
+
+    @Test
+    public void isAdjacentToExistingWord_validCrossing() {
+        BoardLayout layout = new BoardLayout(5, 3);
+        layout.copyLayoutFromStringArray(new String[][]{
+                ".....".split(""),
+                ".pie.".split(""),
+                ".....".split(""),
+        });
+        LaidWord wordToLay = new LaidWord("lie", new BoardPosition(2, 0), Direction.VERTICAL);
+        assertFalse(layout.isAdjacentToExistingWord(wordToLay));
+    }
+
+    @Test
+    public void isAdjacentToExistingWord_accidentalContinuation() {
+        BoardLayout layout = new BoardLayout(5, 1);
+        layout.copyLayoutFromStringArray(new String[][]{
+                "pie..".split("")
+        });
+        LaidWord wordToLay = new LaidWord("eel", new BoardPosition(2, 0), Direction.HORIZONTAL);
+        assertTrue(layout.isAdjacentToExistingWord(wordToLay));
+    }
+
+    // TODO suspicion: additions that don't fully fit on the existing layout aren't handled properly. Test specifically.
+
+    // TODO: Use this to debug the generation in https://pastebin.com/HmEB57w0. This is a rough recreation.
+    @Test
+    public void isAdjacentToExistingWord_horizontalMerge() {
+        BoardLayout layout = new BoardLayout(0, 0);
+        layout.copyLayoutFromStringArray(new String[][]{
+                "..u..".split(""),
+                "..s..".split(""),
+                ".cede".split(""),
+        });
+        LaidWord wordToLay = new LaidWord("sis", new BoardPosition(0, 1), Direction.HORIZONTAL);
+        assertTrue(layout.isAdjacentToExistingWord(wordToLay));
+    }
+
+    // TODO: Use this to debug the generation at https://pastebin.com/eQQKRquc.
+    @Test
+    public void isAdjacentToExistingWord_verticalMerge() {
+        BoardLayout layout = new BoardLayout(6, 7);
+        layout.copyLayoutFromStringArray(new String[][]{
+                "..s...".split(""),
+                "..a...".split(""),
+                "caused".split(""),
+                "a.c...".split(""),
+                "u.e...".split(""),
+                "s.....".split(""),
+                "e.....".split(""),
+        });
+        LaidWord wordToLay = new LaidWord("aces", new BoardPosition(3, -1), Direction.VERTICAL);
+        assertTrue(layout.isAdjacentToExistingWord(wordToLay));
+    }
 }
