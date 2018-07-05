5590148166ea8fab7353a07b4185f6a765248364
animatinator <animatinator@gmail.com>
2018-07-05 14:16:21 +0100

    Add commentary

--- crossword/src/main/java/com/animatinator/crossword/generate/BoardGenerationFlagConstant.java
+++ crossword/src/main/java/com/animatinator/crossword/generate/BoardGenerationFlagConstant.java
@@ -1,6 +1,8 @@
 package com.animatinator.crossword.generate;
 
 public enum BoardGenerationFlagConstant {
+    // Randomise the orientation of the first word placed.
     RANDOM_INITIAL_ORIENTATION,
+    // This flag doesn't control generation; we just use it to test the flags class.
     UNUSED_TEST_FLAG,
 }
