7ffe6ba57cfb23db51259f9318f0e67e925ba513
animatinator <animatinator@gmail.com>
2018-07-12 23:57:04 +0100

    Fingerprints shouldn't have duplicate letters
    Changed my mind - in the common case, these should ignore duplicate
    letters. Actual letter counts will come in later.

--- crossword-dictionary/src/main/java/com/animatinator/crossword/dictionary/fingerprint/FingerPrinter.java
+++ crossword-dictionary/src/main/java/com/animatinator/crossword/dictionary/fingerprint/FingerPrinter.java
@@ -1,11 +1,29 @@
 package com.animatinator.crossword.dictionary.fingerprint;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 class FingerPrinter {
     static WordFingerPrint getFingerprint(String word) {
         String[] characters = word.split("");
         Arrays.sort(characters);
-        return new WordFingerPrint(characters);
+        return new WordFingerPrint(removeDuplicates(characters));
+    }
+
+    private static String[] removeDuplicates(String[] sortedCharacters) {
+        List<String> uniqueCharacters = new ArrayList<>();
+
+        String last = "";
+        for (String character : sortedCharacters) {
+            if (!character.equals(last)) {
+                last = character;
+                uniqueCharacters.add(character);
+            }
+        }
+
+        String[] result = new String[uniqueCharacters.size()];
+        uniqueCharacters.toArray(result);
+        return result;
     }
 }
--- crossword-dictionary/src/test/java/com/animatinator/crossword/dictionary/fingerprint/FingerPrinterTest.java
+++ crossword-dictionary/src/test/java/com/animatinator/crossword/dictionary/fingerprint/FingerPrinterTest.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 @RunWith(JUnit4.class)
 public class FingerPrinterTest {
@@ -15,7 +16,7 @@ public class FingerPrinterTest {
 
     @Test
     public void simpleFingerPrint() {
-        assertEquals("estt", FingerPrinter.getFingerprint("test").toString());
+        assertEquals("est", FingerPrinter.getFingerprint("test").toString());
     }
 
     /**
@@ -23,14 +24,24 @@ public class FingerPrinterTest {
      */
     @Test
     public void spanishWords() {
-        assertEquals("aeinnrtté", FingerPrinter.getFingerprint("intentaré").toString());
+        assertEquals("aeinrté", FingerPrinter.getFingerprint("intentaré").toString());
         assertEquals("cmoí", FingerPrinter.getFingerprint("comí").toString());
         assertEquals("inoñ", FingerPrinter.getFingerprint("niño").toString());
         assertEquals("éíñ", FingerPrinter.getFingerprint("ñéí").toString());
     }
 
     @Test
+    public void nonMatch() {
+        assertNotEquals(FingerPrinter.getFingerprint("hello"), FingerPrinter.getFingerprint("hallo"));
+    }
+
+    @Test
     public void equalFingerPrints() {
         assertEquals(FingerPrinter.getFingerprint("cause"), FingerPrinter.getFingerprint("sauce"));
+    }
+
+    @Test
+    public void duplicateLetters() {
+        assertEquals(FingerPrinter.getFingerprint("adder"), FingerPrinter.getFingerprint("read"));
     }
 }
