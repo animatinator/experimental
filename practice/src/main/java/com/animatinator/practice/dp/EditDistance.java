9640b6b5616993eb7cac84afd2ff28bc7a464452
animatinator <animatinator@gmail.com>
2019-02-04 23:40:27 +0000

    Optimisation: use far less memory
    Space is O(n) if we only store the last DP column.

--- practice/src/main/java/com/animatinator/practice/dp/EditDistance.java
+++ practice/src/main/java/com/animatinator/practice/dp/EditDistance.java
@@ -30,12 +30,12 @@ class EditDistance {
     }
 
     static int getLongestSubsequence(String a, String b) {
-        int[][] dp = new int[a.length() + 1][b.length() + 1];
+        int[][] dp = new int[2][b.length() + 1];
 
         for (int i = 1; i <= a.length(); i++) {
             for (int j = 1; j <= b.length(); j++) {
                 if (a.charAt(i - 1) == b.charAt(j - 1)) {
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    dp[i % 2][j] = dp[(i - 1) % 2][j - 1] + 1;
                 }
             }
         }
